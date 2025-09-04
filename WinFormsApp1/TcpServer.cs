345dfe8fad596e50998aebc0b1413467e091d1d0
Onur Kağan Coşkun <79399112+Okaca@users.noreply.github.com>
2025-09-04 08:39:08 +0300

    Update TcpServer.cs

--- WinFormsApp1/TcpServer.cs
+++ WinFormsApp1/TcpServer.cs
@@ -1,10 +1,17 @@
-﻿using System.Net;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using static WinFormsApp1.Form1;
 
 namespace WinFormsApp1
 {
+    // Lightweight model just to extract MessageId
+    public class BaseMessage
+    {
+        public int MessageId { get; set; }
+    }
+
     public class TcpServer
     {
         private TcpListener _listener;
@@ -28,8 +35,7 @@ namespace WinFormsApp1
                     try
                     {
                         var client = await _listener.AcceptTcpClientAsync();
-                        // await HandleClientAsync(client); // only good for single-client server
-                        _ = Task.Run(() => HandleClientAsync(client));  // Run in parallel, do not await
+                        _ = Task.Run(() => HandleClientAsync(client));
                     }
                     catch (Exception ex)
                     {
@@ -64,44 +70,58 @@ namespace WinFormsApp1
                         byte[] data = buffer.Take(bytesRead).ToArray();
                         OnMessageReceived?.Invoke(data);
 
-                        // Determine message type
-                        var text = Encoding.UTF8.GetString(data);
+                        string json = Encoding.UTF8.GetString(data);
 
-                        if (text.Contains("\"FirstName\"")) // Rough indicator of Message1
+                        BaseMessage baseMsg;
+                        try
                         {
-                            var msg2 = new Message2 // random values for msg2
-                            {
-                                UnitRefNumber = 1234,
-                                LocationValidity = 1,
-                                Latitude = 407123456,
-                                Longitude = 293456789,
-                                Altitude = 120
-                            };
-                            var response = MessageHelper.Serialize(msg2);
-                            await stream.WriteAsync(response, 0, response.Length);
-                            // show sent message
-                            Log("[Transmitted]: " + Encoding.UTF8.GetString(response));
+                            baseMsg = JsonSerializer.Deserialize<BaseMessage>(json);
                         }
-                        else if (text.Contains("\"Latitude\"")) // Rough indicator of Message2
+                        catch
                         {
-                            var msg1 = new Message1 // random values for msg1
-                            {
-                                UnitRefNumber = 4321,
-                                FirstName = "Server",
-                                LastName = "Response",
-                                UnitNo = 999,
-                                Rank = Rank.Tegmen
-                            };
-                            var response = MessageHelper.Serialize(msg1);
-                            await stream.WriteAsync(response, 0, response.Length);
-                            // show sent message
-                            Log("[Transmitted]: " + Encoding.UTF8.GetString(response));
-                        }
-                        else
-                        {
-                            Log("Received unknown message.");
-                            var error = Encoding.UTF8.GetBytes("Unrecognized message");
+                            Log("Failed to parse MessageId.");
+                            var error = Encoding.UTF8.GetBytes("Invalid message format");
                             await stream.WriteAsync(error, 0, error.Length);
+                            continue;
+                        }
+
+                        switch (baseMsg.MessageId)
+                        {
+                            case 1:
+                                // Client sent Message1, respond with Message2
+                                var msg2 = new Message2
+                                {
+                                    UnitRefNumber = 1234,
+                                    LocationValidity = 1,
+                                    Latitude = 407123456,
+                                    Longitude = 293456789,
+                                    Altitude = 120
+                                };
+                                var response2 = MessageHelper.Serialize(msg2);
+                                await stream.WriteAsync(response2, 0, response2.Length);
+                                LogWithTimestamp("Transmitted", response2);
+                                break;
+
+                            case 2:
+                                // Client sent Message2, respond with Message1
+                                var msg1 = new Message1
+                                {
+                                    UnitRefNumber = 4321,
+                                    FirstName = "Server",
+                                    LastName = "Response",
+                                    UnitNo = 999,
+                                    Rank = Rank.Tegmen
+                                };
+                                var response1 = MessageHelper.Serialize(msg1);
+                                await stream.WriteAsync(response1, 0, response1.Length);
+                                LogWithTimestamp("Transmitted", response1);
+                                break;
+
+                            default:
+                                Log($"Unknown MessageId: {baseMsg.MessageId}");
+                                var unknown = Encoding.UTF8.GetBytes("Unknown MessageId");
+                                await stream.WriteAsync(unknown, 0, unknown.Length);
+                                break;
                         }
                     }
                 }
@@ -114,6 +134,13 @@ namespace WinFormsApp1
 
             client.Close();
             Log("Client disconnected.");
+        }
+
+        private void LogWithTimestamp(string label, byte[] data)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string json = Encoding.UTF8.GetString(data);
+            Log($"[{timestamp}] [{label}]: {json}");
         }
 
         private void Log(string message)
